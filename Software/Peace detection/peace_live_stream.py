a8c07fde028fc79ead676c59f7abcd5b3c009396
RubenBuysschaertVives <ruben.buysschaert@vives.be>
2024-12-10 12:00:13 +0100

    Added down counter in image.

--- Software/Peace detection/peace_live_stream.py
+++ Software/Peace detection/peace_live_stream.py
@@ -13,26 +13,32 @@ import math
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
+import time
+
+# Constantes.
+M_AND_M_DELAY = 9
 
 # Globale variabelen.
 result = None
 busy = False
+last_peace_detection = 0
 
 # COM-poort openen naar de Nucleo van het m&m toestel.
-# serialPort = serial.Serial('COM5', 115200)
-serialPort = serial.Serial('/dev/ttyACM0', 115200)
+serial_port = serial.Serial('COM11', 115200)
+# serialPort = serial.Serial('/dev/ttyACM0', 115200)
 
 # Webcam openen via OpenCV.
-cap = cv2.VideoCapture(0)
+capture = cv2.VideoCapture(0)
 
 # Frame grootte opvragen.
-frameWidth = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-frameHeight = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+frame_width = capture.get(cv2.CAP_PROP_FRAME_WIDTH)
+frame_height = capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
-# Create a gesture recognizer instance with the live stream mode:
+# Maak een 'gesture recognizer instance' voor live stream mode.
 def detection_callback(gesture_recognition_result, output_image, timestamp_ms):
     # Globale busy variabele ophalen.
     global busy
+    global last_peace_detection
 
     # Resultaat globaal beschikbaar stellen.
     global result 
@@ -43,10 +49,13 @@ def detection_callback(gesture_recognition_result, output_image, timestamp_ms):
         # print(gesture[0].category_name, timestamp)
         # print()
 
-        # Is er een 'peace sign' gedetecteerd? Data verzenden naar de m&m microcontroller.
+        # Is er een 'peace sign' gedetecteerd en is de wachttijd voorbij? Data verzenden naar de m&m microcontroller.
         if gesture[0].category_name == "Victory": 
-            print("Peace sign detected.", timestamp)               
-            serialPort.write(b'm')
+            print("Peace sign detected.", timestamp)
+            if(time.time() > last_peace_detection + M_AND_M_DELAY):        
+                serial_port.write(b'm')
+                print("m&m request sent to microcontroller.")
+                last_peace_detection = time.time()
 
     # Aangeven dat de verwerking voorbij is (en een nieuwe mag beginnen in de 'hoofdlus').
     busy = False
@@ -54,20 +63,19 @@ def detection_callback(gesture_recognition_result, output_image, timestamp_ms):
 # Werken met 'LIVE_STREAM' i.p.v. 'VIDEO' om zo meer real time te werken.
 # Zie: https://ai.google.dev/edge/mediapipe/solutions/vision/gesture_recognizer/python#live-stream
 # Gesture recognizer objecten maken.
-VisionRunningMode = mp.tasks.vision.RunningMode
 # OPM: pad kan aanpassingen vereisen als je op de Raspberry Pi werkt (Windows/Linux).
-# gesture_recognizer_base_options = python.BaseOptions(model_asset_path='gesture_recognizer.task')
-gesture_recognizer_base_options = python.BaseOptions(model_asset_path='Software/Peace detection/gesture_recognizer.task')
+gesture_recognizer_base_options = python.BaseOptions(model_asset_path='gesture_recognizer.task')
+# gesture_recognizer_base_options = python.BaseOptions(model_asset_path='Software/Peace detection/gesture_recognizer.task')
 gesture_recognizer_options = vision.GestureRecognizerOptions(base_options=gesture_recognizer_base_options,
-                                        running_mode=VisionRunningMode.LIVE_STREAM,
+                                        running_mode=mp.tasks.vision.RunningMode.LIVE_STREAM,
                                         num_hands=2,
                                         result_callback=detection_callback)
 gesture_recognizer = vision.GestureRecognizer.create_from_options(gesture_recognizer_options)
 
 # Indien video beschikbaar...
-while(cap.isOpened()):
+while(capture.isOpened()):
     # Volgende frame inlezen.
-    ret, frame = cap.read()
+    ret, frame = capture.read()
 
     # Beeld spiegelen.
     frame = cv2.flip(frame, 1)
@@ -75,22 +83,37 @@ while(cap.isOpened()):
     # Convert the frame received from OpenCV to a MediaPipe’s Image object.
     mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
 
-    # Detectie doen op 'video' (als er geen meer bezig is).
-    timestamp = int(cap.get(cv2.CAP_PROP_POS_MSEC))
+    # Detectie doen op 'live stream' (als er geen detectie meer bezig is).
+    timestamp = int(capture.get(cv2.CAP_PROP_POS_MSEC))
     if busy == False:
         gesture_recognizer.recognize_async(mp_image, timestamp)
     busy = True
 
-    # Detectie landmarks tekenen.
+    # Detectie landmarks tekenen als er gevonden werden.
     if result != None:
         for hand_landmarks_one_hand in result.hand_landmarks:
             for landmark in hand_landmarks_one_hand:                
-                x = int(landmark.x * frameWidth)
-                y = int(landmark.y * frameHeight)
+                x = int(landmark.x * frame_width)
+                y = int(landmark.y * frame_height)
                 # Z geeft een indicatie van volgorde in Z-as. Groter is dichter bij de camera.
                 # print("Z: ", landmark.z)
-                cv2.circle(frame, (x,y), 3, (0, 255, 0), 3)
 
+                if(time.time() > last_peace_detection + M_AND_M_DELAY):
+                    # Groene cirkels tekenen als het langer dan 10 seconden geleden is dat er een m&m werd afgeleverd.
+                    cv2.circle(frame, (x,y), 3, (0, 255, 0), 3)
+                else:
+                    # Rode cirkels tekenen als het minder lang dan 10 seconden geleden is dat er een m&m werd afgeleverd.
+                    cv2.circle(frame, (x,y), 3, (0, 0, 255), 3)
+
+    # Berekenen hoe lang het nog duurt vooraleer er een nieuw m&m mag uitgedeeld worden.
+    waiting_time = last_peace_detection + M_AND_M_DELAY - time.time()
+    # Is de wachttijd voorbij? Toon nul in het groen. Anders de resterende seconden in het rood.
+    if waiting_time < 0:
+        waiting_time = 0
+        waiting_time_color = (0,255,0)      # Groen.
+    else:
+        waiting_time_color = (0,0,255)      # Rood.
+    cv2.putText(frame, f"{waiting_time:.1f}", org=(int(frame_width - 70), int(frame_height - 30)), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=waiting_time_color, thickness=4)
     # Beeld tonen.
     cv2.imshow("Gesture recognition with Mediapipe and OpenCV", frame)
 
@@ -99,6 +122,6 @@ while(cap.isOpened()):
         break
 
 # Alle resources vrijgeven.
-cap.release()
+capture.release()
 cv2.destroyAllWindows()
-serialPort.close()
+serial_port.close()
